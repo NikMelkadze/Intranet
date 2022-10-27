abef6a605457ab9343b84803c91680d8ff09adb7
NikMelkadze <n_melkadze@cu.edu.ge>
2022-10-27 23:24:23 +0400

    added img url to getEmployee and Employee

--- Intranet.Application/Employee/Queries/GetEmployees/GetEmployeesQuery.cs
+++ Intranet.Application/Employee/Queries/GetEmployees/GetEmployeesQuery.cs
@@ -2,11 +2,13 @@
 using Intranet.Application.User.Registration;
 using Intranet.Persistance.Models;
 using MediatR;
+using Microsoft.AspNetCore.Http;
 
 namespace Intranet.Application.User.GetUser
 {
     public class GetEmployeesQuery : IRequest<GetEmployeesResponse>
     {
+        public HttpContext HttpContext { get; set; }
     }
     public class GetEmployeesResponse
     {
